4fee116eaedb9af4446ef0d63c3e6840fcf0fb32
oreze <kunas1@o2.pl>
2021-04-21 22:30:54 +0200

    Add weather input form
    Add input form to enter location and type of weather that user wants to check (currently only "current" mode available)

---
+++ Controllers/WeatherController.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Forecaster.Models.OpenWeather;
+using Forecaster.Services;
+using Microsoft.AspNetCore.Mvc;
+using Serilog;
+
+namespace Forecaster.Controllers
+{
+    public class WeatherController : Controller
+    {
+        private readonly OpenWeatherService _openWeather;
+        // GET: Forecaster/Index
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/OpenWeather/ModeEnum.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace Forecaster.Models.OpenWeather
+{
+    public enum ModeEnum
+    {
+        [Display(Name = "Current weather")]
+        Current
+    }
+}
