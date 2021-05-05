97b212c5e645bd903f96ae235cc93f68d36da657
oreze <kunas1@o2.pl>
2021-05-06 00:00:24 +0200

    Add SCSS support, upgrade bootstrap to 5
    Added SCSS support with auto compiling .scss + bootstrap before building. It doesn`t work for "dotnet watch run" if .scss is changed.

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+﻿const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+const bundleFileName = 'bundle';
+const dirName = 'wwwroot/dist';
+
+module.exports = (env, argv) => {
+    return {
+        mode: argv.mode === "production" ? "production" : "development",
+        entry: ['./node_modules/bootstrap/dist/js/bootstrap.bundle.js', './wwwroot/js/site.js', './wwwroot/scss/site.scss'],
+        output: {
+            filename: bundleFileName + '.js',
+            path: path.resolve(__dirname, dirName)
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.s[c|a]ss$/,
+                    use:
+                        [
+                            'style-loader',
+                            MiniCssExtractPlugin.loader,
+                            'css-loader',
+                            {
+                                loader: 'postcss-loader', // Run postcss actions
+                                options: {
+                                    postcssOptions: {
+                                        plugins: function () { // postcss plugins, can be exported to postcss.config.js
+
+                                            let plugins = [require('autoprefixer')];
+
+                                            if (argv.mode === "production") {
+
+                                                plugins.push(require('cssnano'));
+                                            }
+
+                                            return plugins;
+                                        }
+                                    }
+                                }
+                            },
+                            'sass-loader'
+                        ]
+                }
+            ]
+        },
+        plugins: [
+            new CleanWebpackPlugin(),
+            new MiniCssExtractPlugin({
+                filename: bundleFileName + '.css'
+            })
+        ]
+    };
+};
